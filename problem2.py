1dfb076c912e74d16c2cd63aca172ada228a5f80
Harsh Mangal <harsh.mangal108@gmail.com>
2019-06-11 19:32:30 +0000

    working with urls

---
+++ problem2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import webbrowser
+from googlesearch import search
+
+f=open("urls.txt","w")
+web = input("Enter topic to search : ")
+x=search(web,stop=10)
+for i in x:
+	f.write(i+"\n")
+webbrowser.open("https://www.google.com/search?q="+web)
+
+
